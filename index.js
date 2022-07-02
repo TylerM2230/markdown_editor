64bb4e0ad8287d34b1dd3bd05b0beeceeea8bad3
Tyler M <tymiranda08@gmail.com>
2022-07-01 19:37:11 -0800

    add default text

---
+++ index.js
@@ -0,0 +1,17 @@
+function updatePreview() {
+  let previewElement = document.getElementById("preview");
+  let editorValue = document.getElementById("editor").value;
+  previewElement.innerHTML = marked.parse(editorValue);
+}
+
+function setDefault() {
+  let previewElement = document.getElementById("preview");
+  let editorView = document.getElementById("editor");
+  const defaultText = `\
+  # This is an H1 heading
+  ## This is an H2 heading
+  ** This is Bolded Text **
+  `;
+  previewElement.innerHTML = marked.parse(defaultText);
+  editorView.value = defaultText;
+}
